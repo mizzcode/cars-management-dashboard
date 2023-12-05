8179f9a548d48715e39e0544df7445c932182882
Misbahudin <misbahudin.id@gmail.com>
2023-12-05 14:58:00 +0700

    feat: offcanvas component

--- src/components/react-bootstrap/offcanvas.tsx
+++ src/components/react-bootstrap/offcanvas.tsx
@@ -6,9 +6,15 @@ import { useState } from 'react'
 type OffCanvas = {
     btnIcon: string
     placement: string
+    title: string
+    link1: string
+    link2: string
+    link3: string
+    link4: string
+    link5: string
 }
 
-function OffCanvas({ btnIcon, ...props }: OffCanvas) {
+function OffCanvas({ btnIcon, title, link1, link2, link3, link4, link5, ...props }: OffCanvas) {
     const [show, setShow] = useState(false)
 
     const handleClose = () => setShow(false)
@@ -19,34 +25,34 @@ function OffCanvas({ btnIcon, ...props }: OffCanvas) {
             {/* @ts-expect-error */}
             <Offcanvas show={show} onHide={handleClose} {...props}>
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Binar Car Rental</Offcanvas.Title>
+                    <Offcanvas.Title>{title}</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                     <ul>
                         <li className='mb-3'>
                             <a href='#our-services' className='fw-semibold' onClick={handleClose}>
-                                Our Services
+                                {link1}
                             </a>
                         </li>
                         <li className='mb-3'>
                             <a href='#why-us' className='fw-semibold' onClick={handleClose}>
-                                Why Us
+                                {link2}
                             </a>
                         </li>
                         <li className='mb-3'>
                             <a href='#testimonial' className='fw-semibold' onClick={handleClose}>
-                                Testimonial
+                                {link3}
                             </a>
                         </li>
                         <li className='mb-3'>
                             <a href='#faq' className='fw-semibold' onClick={handleClose}>
-                                FAQ
+                                {link4}
                             </a>
                         </li>
                     </ul>
                     <Button>
                         <a href='#' onClick={handleClose}>
-                            Register
+                            {link5}
                         </a>
                     </Button>
                 </Offcanvas.Body>
