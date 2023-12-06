f6eea1bc9ec6d572e0c722fc3f73379d9de569c2
Misbahudin <misbahudin.id@gmail.com>
2023-12-07 01:36:02 +0700

    bug: login

--- src/context/user.tsx
+++ src/context/user.tsx
@@ -5,13 +5,17 @@ import httpFetch from '../utils/fetch'
 
 export type TUserContext = {
     user: User | undefined
-    saveUser: (info: User) => void
 }
 export const UserContext = createContext<ContextType<Context<TUserContext>> | null>(null)
 
 // @ts-expect-error wrong types
 const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
     const [user, setUser] = useState<User | undefined>()
+
+    /**
+     * note: alih-alih login mendapatkan token kemudian get profile dengan token nya tapi malah get profile dulu baru post login
+     * hasilnya invalid token, maka harus 2x login atau bisa langsung ke url dashboard, dan bekerja
+     */
 
     const navigate = useNavigate()
     useEffect(() => {
@@ -30,11 +34,7 @@ const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
         getUser()
     }, [navigate])
 
-    function saveUser(info: User) {
-        setUser(info)
-    }
-
-    return <UserContext.Provider value={{ user, saveUser }}>{children}</UserContext.Provider>
+    return <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
 }
 
 export default UserProvider
