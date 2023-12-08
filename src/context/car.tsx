6d10b4fbe5e14696bd73f2cdae37bc91415fc433
Misbahudin <misbahudin.id@gmail.com>
2023-12-08 22:40:51 +0700

    feat: create context cars

--- src/context/car.tsx
+++ src/context/car.tsx
@@ -4,26 +4,25 @@ import { useNavigate } from 'react-router-dom'
 import httpFetch from '@/utils/fetch'
 
 export type TCarContext = {
-    car: Car | undefined
+    cars: Car | undefined
 }
 export const CarContext = createContext<ContextType<Context<TCarContext>> | null>(null)
 
 // @ts-expect-error wrong types
 const CarProvider: React.FC<React.ReactNode> = ({ children }) => {
-    const [car, setCar] = useState<Car | undefined>()
+    const [cars, setCars] = useState<Car[] | undefined>()
 
     const navigate = useNavigate()
     useEffect(() => {
         async function getCar() {
-            // TODO: get cars by name
-            await httpFetch('cars', {}, {}, true)
+            await httpFetch('cars', {}, {}, false)
                 .then((res) => {
                     if (!res.ok) throw new Error('Invalid token!')
                     return res.json()
                 })
                 .then((data) => {
                     console.log(data)
-                    setCar(data)
+                    setCars(data)
                 })
                 .catch((err) => {
                     console.error(err)
@@ -33,7 +32,8 @@ const CarProvider: React.FC<React.ReactNode> = ({ children }) => {
         getCar()
     }, [navigate])
 
-    return <CarContext.Provider value={{ car }}>{children}</CarContext.Provider>
+    // @ts-expect-error cars
+    return <CarContext.Provider value={{ cars }}>{children}</CarContext.Provider>
 }
 
 export default CarProvider
