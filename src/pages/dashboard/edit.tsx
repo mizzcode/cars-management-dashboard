fd60645603e151d9beeccf10b83099abcf9d77aa
Misbahudin <misbahudin.id@gmail.com>
2023-12-11 00:45:44 +0700

    feat: page edit car

---
+++ src/pages/dashboard/edit.tsx
@@ -0,0 +1,12 @@
+import FormEditCar from '@/components/dashboard/form-edit'
+
+function EditCar() {
+    return (
+        <>
+            <h1 className='m-3'>Edit Car</h1>
+            <FormEditCar />
+        </>
+    )
+}
+
+export default EditCar
