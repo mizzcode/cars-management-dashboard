6898467ce41d057ec309cde2c5a027fe7e86a6fb
Misbahudin <misbahudin.id@gmail.com>
2023-12-10 19:46:38 +0700

    feat: page add car

---
+++ src/pages/dashboard/add.tsx
@@ -0,0 +1,12 @@
+import FormAddCar from '@/components/dashboard/form-add'
+
+function AddCar() {
+    return (
+        <>
+            <h1 className='m-3'>Add New Car</h1>
+            <FormAddCar />
+        </>
+    )
+}
+
+export default AddCar
